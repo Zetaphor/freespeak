c6ebb7dc1e6986919102dff776259e7c537439bf
Zetaphor <zetaphor@hey.com>
2025-04-10 10:25:26 -0500

    Set pwd in launch

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-source venv/bin/activate
+# Set the working directory to the directory of the script
+cd "$(dirname "$0")"
+
+source .venv/bin/activate
 # Run the Flask application in the background and discard output
 python src/main.py > /dev/null 2>&1 &
